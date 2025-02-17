40272d9c9ed2146d565374bc409c1cff02bc97b2
MateoSebF <mateo.forero-f@mail.escuelaing.edu.co>
2025-02-16 20:56:01 -0500

    feat: comment line

--- src/main/java/co/edu/eci/pigball/game/GameApplication.java
+++ src/main/java/co/edu/eci/pigball/game/GameApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GameApplication {
 
 	public static void main(String[] args) {
+		//Comment
 		SpringApplication.run(GameApplication.class, args);
 	}
 
