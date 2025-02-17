5002f5a1a73ec3874b2fc75f666ffabc625256bb
MateoSebF <mateo.forero-f@mail.escuelaing.edu.co>
2025-02-16 20:27:59 -0500

    feat: intital version

---
+++ src/test/java/co/edu/eci/pigball/game/GameApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.eci.pigball.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
