33fc10c3b37a34f212ba338c9c4236c1d34b81ec
MateoSebF <mateo.forero-f@mail.escuelaing.edu.co>
2025-03-07 22:40:38 -0500

    feat:  initial base concurrent players

--- src/main/java/co/edu/eci/pigball/game/GameApplication.java
+++ src/main/java/co/edu/eci/pigball/game/GameApplication.java
@@ -2,8 +2,10 @@ package co.edu.eci.pigball.game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling  // This enables the execution of @Scheduled methods
 public class GameApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/co/edu/eci/pigball/game/controller/GameController.java
@@ -0,0 +1,58 @@
+package co.edu.eci.pigball.game.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+
+import co.edu.eci.pigball.game.model.Game;
+import co.edu.eci.pigball.game.model.Movement;
+import co.edu.eci.pigball.game.model.Player;
+import co.edu.eci.pigball.game.model.DTO.GameDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+@Controller
+@Getter
+@Setter
+public class GameController {
+    @Autowired
+    private Game game;
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate; // Inject messaging template
+
+    @MessageMapping("/play")
+    public void makeAMovement(Movement movement) {
+        game.makeAMove(movement.getPlayer(), movement.getDx(), movement.getDy());
+        // No need to return anything; state is sent periodically
+    }
+
+    @Scheduled(fixedRate = 50)
+    public void broadcastGameState() {
+        try {
+            GameDTO gameDTO = game.getGameDTO();
+            if (gameDTO == null) {
+                return; // Prevent sending null messages
+            }
+            messagingTemplate.convertAndSend("/topic/play", gameDTO);
+        } catch (Exception e) {
+        }
+    }
+
+    @MessageMapping("/join")
+    @SendTo("/topic/playerJoined")
+    public List<Player> handleNewPlayer(Player player) {
+        System.out.println("New player joined: " + player.getName());
+
+        // Add player to the game
+        game.addPlayer(player.getName(), player);
+
+        // Return the updated list of all players in the game
+        return game.getAllPlayers();
+    }
+}
