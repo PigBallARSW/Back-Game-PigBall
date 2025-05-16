21517ee5c07ba7c9fdec6b5cb6251b9876773a6c
mateoSebF <mateo.forero-f@mail.escuelaing.edu.co>
2025-05-16 16:43:57 -0500

    feat: health check

--- src/main/java/co/edu/eci/pigball/game/controller/LobbyController.java
+++ src/main/java/co/edu/eci/pigball/game/controller/LobbyController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.*;
 import co.edu.eci.pigball.game.exception.GameException;
 import co.edu.eci.pigball.game.model.dto.GameDTO;
 import co.edu.eci.pigball.game.service.GameService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class LobbyController {
@@ -16,6 +19,12 @@ public class LobbyController {
     public LobbyController(GameService gameService) {
         this.gameService = gameService;
     }
+
+    @GetMapping("/health")
+    public boolean getMethodName(@RequestParam String param) {
+        return true;
+    }
+    
 
     @PostMapping("/createGame")
     public ResponseEntity<Object> createGame(
