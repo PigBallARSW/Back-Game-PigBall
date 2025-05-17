65de374f9b12c8a7b4b6c9a6ce79ee04b947c90c
Jaider Vargas <jaider.vargas-n@mail.escuelaing.edu.co>
2025-05-17 03:02:07 -0500

    fix: repair verify

--- src/main/java/co/edu/eci/pigball/game/model/store/RedisGameStore.java
+++ src/main/java/co/edu/eci/pigball/game/model/store/RedisGameStore.java
@@ -21,7 +21,7 @@ import org.springframework.stereotype.Component;
 import co.edu.eci.pigball.game.exception.GameException;
 import co.edu.eci.pigball.game.model.Game;
 
-@Component
+//@Component
 @ConditionalOnProperty(name = "app.store.type", havingValue = "redis")
 public class RedisGameStore implements IGameStore {
 
